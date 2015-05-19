c3d153f4d73e54c24c21f380092fdbb07dca37f4
msbrandt <msbrandt@syr.edu>
2015-05-19 14:19:03 -0400

    Added favicon

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no width=device-width" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
 	<title>
 		<?php
